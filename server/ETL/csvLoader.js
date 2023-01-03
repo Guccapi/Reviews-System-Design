ad77fd32f3bad05591e42bf753ef6e897ea314c0
Matt <siglerm37@gmail.com>
2023-01-02 18:56:25 -0800

    Seeding Reviews created and fixed Schema

--- server/ETL/csvLoader.js
+++ server/ETL/csvLoader.js
@@ -1,22 +1,133 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 const path = require('path');
-const { Pool } = require('pg');
+const fns = require('date-fns');
+const pgp = require('pg-promise')({});
 
-const db = new Pool({
+const cn = {
   host: 'localhost',
   user: 'elizabeth',
   pssword: 'Empireswillriseagain',
   port: 5432,
   database: 'GucciRR',
-  allowExitOnIdle: true,
-});
+};
 
-const characteristicsReview = [];
+const db = pgp(cn);
+const reviews = [];
+const reviewColumns = new pgp.helpers.ColumnSet(
+  [
+    'review_id',
+    'product_id',
+    'rating',
+    'date',
+    'summary',
+    'body',
+    'recommend',
+    'reported',
+    'reviewer_name',
+    'reviewer_email',
+    'response',
+    'helpfulness',
+    // 'photos',
+  ],
+  { table: 'review' },
+);
+
+const queryReview = async (query1, query2, query3, query4) => {
+  try {
+    await db.any(query1);
+  } catch (err) {
+    console.error(err);
+  } try {
+    await db.any(query2);
+  } catch (err) {
+    console.error(err);
+  } try {
+    await db.any(query3);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await db.any(query4);
+  }
+};
 
 fs.createReadStream(path.join(__dirname, '../CSVS/reviews.csv'))
   .pipe(csv())
-  .on('data', (data) => console.log(data))
+  .on('data', async (data) => {
+    const review_id = Number(data.id);
+    const product_id = Number(data.product_id);
+    const rating = Number(data.rating);
+    const date = fns.fromUnixTime(data.date / 1000).toISOString();
+    const summary = data.summary.replaceAll("'", "''");
+    const body = data.body.replaceAll("'", "''");
+    const { recommend } = data;
+    const { reported } = data;
+    const reviewer_name = data.reviewer_name.replaceAll("'", "''");
+    const reviewer_email = data.reviewer_email.replaceAll("'", "''");
+    const response = data.response.replaceAll("'", "''");
+    const helpfulness = Number(data.helpfulness);
+    reviews.push({
+      review_id,
+      product_id,
+      rating,
+      date,
+      summary,
+      body,
+      recommend,
+      reported,
+      reviewer_name,
+      reviewer_email,
+      response,
+      helpfulness,
+    });
+  })
   .on('end', () => {
-    console.log(characteristicsReview);
+    const insertion1 = pgp.helpers.insert(reviews.slice(0, reviews.length / 4), reviewColumns);
+    const insertion2 = pgp.helpers.insert(
+      reviews.slice(reviews.length / 4, (reviews.length / 2)),
+      reviewColumns,
+    );
+    const insertion3 = pgp.helpers.insert(
+      reviews.slice(reviews.length / 2, ((reviews.length / 4) * 3)),
+      reviewColumns,
+    );
+    const insertion4 = pgp.helpers.insert(reviews.slice(-(reviews.length / 4)), reviewColumns);
+
+    const t0 = performance.now();
+    queryReview(insertion1, insertion2, insertion3, insertion4);
+    const t1 = performance.now();
+    console.log(`Reviews took ${t1 - t0} milliseconds to complete!`);
+  })
+  .on('error', (err) => {
+    console.error(err);
   });
+
+// const characteristicsArray = [];
+// const characteristicsColumns = new pgp.helpers.ColumnSet(
+//   [
+//     'product_id',
+//     // 'ratings',
+//     // 'recommended',
+//     // 'characteristics',
+//   ],
+//   { table: 'productMeta' },
+// );
+
+// fs.createReadStream(path.join(__dirname, '../CSVS/characteristics.csv'))
+//   .pipe(csv())
+//   .on('data', async (data) => {
+//     const characteristicId = Number(data.id);
+//     const characteristicName = data.name.replaceAll("'", "''");
+//     const productId = Number(data.product_id);
+//     characteristicsArray.push(productId);
+//   })
+//   .on('end', () => {
+//     const insertion = pgp.helpers.insert(characteristicsArray, characteristicsColumns);
+//     const t0 = performance.now();
+//     db.none(insertion);
+//     const t1 = performance.now();
+//     console.log(`Characteristics took ${t1 - t0} milliseconds to complete`);
+//   })
+//   .on('error', (err) => {
+//     console.error(err);
+//   });
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,29 +1,20 @@
-DROP TABLE IF EXISTS "productinfo" CASCADE;
-
-CREATE TABLE "productinfo" (
-	"Product_id" serial NOT NULL,
-	"Review_id" int NOT NULL,
-	CONSTRAINT "Product Info_pk" PRIMARY KEY ("Product_id")
-) WITH (
-  OIDS=FALSE
-);
-
 DROP TABLE IF EXISTS "review" CASCADE;
 
 CREATE TABLE "review" (
-	"review_id" serial NOT NULL,
-	"rating" serial NOT NULL,
-	"summary" serial NOT NULL,
-	"recommended" serial NOT NULL,
-	"response" serial NOT NULL,
-	"body" TEXT NOT NULL,
-	"date" timestamp with time zone NOT NULL,
-	"reviewer_name" varchar(60) NOT NULL,
-	"helpfulness" int NOT NULL,
-	"photos" jsonb NOT NULL,
-	"reported" BOOLEAN NOT NULL,
+	"review_id" int NOT NULL,
 	"product_id" int NOT NULL,
-	CONSTRAINT "reviews_pk" PRIMARY KEY ("review_id")
+	"rating" int NOT NULL,
+	"date" timestamptz NOT NULL,
+	"summary" TEXT NOT NULL,
+	"body" TEXT NOT NULL,
+	"recommend" TEXT NOT NULL,
+	"reported" BOOLEAN NOT NULL,
+	"reviewer_name" varchar(60) NOT NULL,
+	"reviewer_email" varchar(60) NOT NULL,
+	"response" TEXT,
+	"helpfulness" int NOT NULL,
+	"photos" jsonb,
+	CONSTRAINT "review_pk" PRIMARY KEY ("review_id")
 ) WITH (
   OIDS=FALSE
 );
@@ -31,21 +22,13 @@ CREATE TABLE "review" (
 DROP TABLE IF EXISTS "productmeta" CASCADE;
 
 CREATE TABLE "productmeta" (
-	"product_id" serial NOT NULL,
-	"ratings" jsonb NOT NULL,
-	"recommended" jsonb NOT NULL,
-	"Characteristics" jsonb NOT NULL,
-	CONSTRAINT "Product Meta_pk" PRIMARY KEY ("product_id")
+	"product_id" int NOT NULL,
+	"ratings" jsonb,
+	"recommend" jsonb,
+	"characteristics" jsonb NOT NULL,
+	CONSTRAINT "productmeta_pk" PRIMARY KEY ("product_id")
 ) WITH (
   OIDS=FALSE
 );
-
-
-ALTER TABLE "productinfo" ADD CONSTRAINT "productinfo_fk0" FOREIGN KEY ("Review_id") REFERENCES "review"("review_id");
-
-ALTER TABLE "review" ADD CONSTRAINT "review_fk0" FOREIGN KEY ("product_id") REFERENCES "productinfo"("Product_id");
-
-ALTER TABLE "productmeta" ADD CONSTRAINT "productmeta_fk0" FOREIGN KEY ("product_id") REFERENCES "productinfo"("Product_id");
-
 
 
